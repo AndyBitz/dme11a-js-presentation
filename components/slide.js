349696be604ee526e1755bd63ac23904153ee596
Andy Bitz <artzbitz13+git@gmail.com>
2017-05-06 14:34:36 +0200

    no click handler for changing slides

--- components/slide.js
+++ components/slide.js
@@ -7,23 +7,12 @@ export default class Slide extends Component {
   constructor(props) {
     super(props);
     this.props = props;
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(e) {
-    if (e.pageX > document.body.clientWidth/2) {
-      // next slide
-      (this.props.next) ? Router.push(this.props.next) : null;
-    } else {
-      // prev slide
-      (this.props.prev) ? Router.push(this.props.prev) : null;        
-    }
   }
 
   render() {
     return (
       <Layout>
-        <article onClick={this.handleClick}>
+        <article>
           { this.props.children }
           <style jsx>
             {`

35bb6992555cfdc62fb9aef37404ea3d71935b57
Andy Bitz <artzbitz13+git@gmail.com>
2017-05-08 21:25:43 +0200

    removed emoji log from server

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ io.on('connection', socket => {
     socket.broadcast.emit('viewer-update', data);
   });
   socket.on('viewer-emoji', data => {
-    console.log('got emoji from viewer');
     io.emit('host-emoji-update', data);
   });
 });

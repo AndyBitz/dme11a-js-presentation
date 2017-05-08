6c3244d226ef18082d68a911d2f517222479ef88
Andy Bitz <artzbitz13+git@gmail.com>
2017-05-09 00:45:58 +0200

    updated slide 0x04

--- pages/slides/0x04_call_by_reference.js
+++ pages/slides/0x04_call_by_reference.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import io from 'socket.io-client';
 import fetch from 'isomorphic-fetch';
 
 import Page from '../../layouts/page.js';
@@ -8,17 +9,51 @@ import Headline from '../../components/headline.js';
 import Enum from '../../components/enum.js';
 import Code from '../../components/code.js'
 import Column from '../../components/column.js';
+import Emojis from '../../components/emojis.js';
+import SlideNavigation from '../../components/slidenavigation.js';
 
-export default class extends Component {
+class SlideFour extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      socket: undefined
+    };
+    this.emojiModule = this.emojiModule.bind(this);
+  }
+
   static async getInitialProps() {
     const response = await fetch('http://localhost:3000/static/html_template.txt');
     const htmlCode = await response.text();
     return { htmlCode };
   }
 
-  constructor(props) {
-    super(props);
-    this.props = props;
+  componentDidMount() {
+    // role
+    if (!window.role) {
+      window.role = 'VISITOR';
+    } else if (window.role !== 'HOST') {
+      window.role = 'VISITOR';
+    }
+    // socket
+    if (!this.state.socket) {
+      const socket = io('http://localhost:3000');
+      socket.on('viewer-update', data => {
+        if (window.role === 'VIEWER') {
+          Router.replace(data.url);
+        }
+      });
+      this.setState(state => ( {socket: socket} ));
+    }
+  }
+
+  emojiModule() {
+    if (!this.state.socket) return null;
+    return (
+      <Emojis
+        socket={this.state.socket}
+      />
+    );
   }
 
   render() {
@@ -34,8 +69,15 @@ export default class extends Component {
           <Column>
             <Code language='html'>{ this.props.htmlCode }</Code>
           </Column>
+          <SlideNavigation
+            prev="/slides/0x03_define"
+            next="/"
+          />
         </Slide>
+        { this.emojiModule() }
       </Page>
     );
   }
 };
+
+export default SlideFour;
